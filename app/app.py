67dc419c45b1b87e40974cba7c4c6e64bd7fc8af
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-10-11 17:24:11 +0000

    test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! from Flask'
 
 if __name__ == '__main__':
     print(hello_world())
