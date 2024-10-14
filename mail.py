846219a1e237cf2b722b3ecd20471cce2b642c6e
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-10-14 17:47:13 +0000

    add mail script

---
+++ mail.py
@@ -0,0 +1,16 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mail = os.getenv('MAIL')
+password = os.getenv('PASSWORD')
+
+smtObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtObj.starttls()
+smtObj.login(mail, password)
+
+smtObj.sendmail(mail, 'mbadassou1@gmail.com', 'Subject: So long.\nDear Mawuli, so long and thanks for all the fish.')
+
+smtObj.quit()
