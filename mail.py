afb03eb23b4afbf564958a58aaed3d5f4c88d7b4
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-10-15 08:44:17 +0000

    print

--- mail.py
+++ mail.py
@@ -12,6 +12,7 @@ print(f'Mail: {mail}, Password: {password}')
 if mail and password:
     smtObj = smtplib.SMTP('smtp.gmail.com', 587)
     smtObj.starttls()
+    print(f'Mail: {mail}, Password: {password}')
     smtObj.login(mail, password)
 
     smtObj.sendmail(mail, 'mbadassou1@gmail.com', 'Subject: So long.\nDear Mawuli, so long and thanks for all the fish.')
